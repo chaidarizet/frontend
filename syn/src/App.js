77b33128667cd49d10bb10c46b7e25be6adc0277
izet <chaidarizet@gmail.com>
2018-09-04 22:29:14 +0700

    Syn shop

---
+++ syn/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import header from './component/views/header.jsx';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+class App extends Component {
+  render() {
+    return (
+
+      <div>
+      <header/>
+      
+      </div>
+    );
+  }
+}
+
+export default App;
